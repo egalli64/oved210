bbd4db1be47c0a42932b5d7c356a09fbd33bc7b9
Manny <egalli64@gmail.com>
2019-02-22 15:55:37 +0100

    WebMVC basic implementation

---
+++ src/main/java/dd/Oved210Application.java
@@ -0,0 +1,13 @@
+package dd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oved210Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oved210Application.class, args);
+	}
+
+}
