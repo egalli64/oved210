57e3dd6d5579ee29cdf0d73a8e306b8d1a3809db
elmonda <gjekmarkaj.elmonda@gmail.com>
2019-02-26 16:18:03 +0100

    guesser

---
+++ src/main/java/elmonda/lopper.java
@@ -0,0 +1,23 @@
+package elmonda;
+
+public class lopper {
+	public static void main(String[] args) {
+		System.out.println("***For loop***");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("hello" + i);
+		}
+
+		int i = 0;
+		while (i < 10) {
+			System.out.println("hello" + i);
+			i += 1;
+		}
+
+		i = 0;
+		do {
+			System.out.println("hello" + i);
+			i += 1;
+
+		} while (i < 10);
+	}
+}
