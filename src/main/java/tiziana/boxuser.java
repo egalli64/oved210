70cadf01dfdea885674d25aeef6b9d38984cf1f2
Tiziana <tizianaleogrande@gmail.com>
2019-02-27 14:37:47 +0100

    voglio salvare recursion

--- src/main/java/tiziana/boxuser.java
+++ src/main/java/tiziana/boxuser.java
@@ -1,15 +1,18 @@
 package tiziana;
 
+import trainer.Box;
+
 public class boxuser {
 	public static void main(String[] args) {
 		
+		System.out.println("How many boxes?"+Box.getCounter());
 		Box box=new Box(10, 10, 10);
 		Box box2=new Box(1, 2, 3);
 		Box box3=new Box();
-	
 		
+		System.out.println("How many boxes? " + Box.getCounter());
 		
-		box.setX(40);
+		box3.setX(40);
 		
 		System.out.println("Box x:"+box.getX());
 		System.out.println("Volume is:"+box.getVolume());
@@ -21,6 +24,9 @@ public class boxuser {
 		CubicbBox cb = new CubicbBox (5);
 
 		System.out.println("CB volume is:"+ cb.getVolume());
+		System.out.println(cb.toString());
+
+		System.out.println("How many boxes? " + Box.getCounter());
 	}
 
 }
