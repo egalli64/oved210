8c155ed26349bc31749546cdd83e7f1b9684939d
Monica <monica.conti93@gmail.com>
2019-02-27 09:06:31 +0100

    lavoro 26/02/19

---
+++ src/main/java/monica/BoxUser.java
@@ -0,0 +1,23 @@
+package monica;
+
+public class BoxUser {
+	public static void main(String[] args) {
+		Box box = new Box(10, 10, 10);
+
+		Box box2 = new Box(1, 2, 3);
+
+		Box box3 = new Box();
+
+		box3.setX(40);
+
+		System.out.println("Box dimentions: " + box.getX());
+		System.out.println("volume is: " + box.getVolume());
+		System.out.println("Box2 is:" + box2.getVolume());
+		System.out.println(" Box3 volume is: " + box3.getVolume());
+
+		System.out.println(box.toString());
+		CubicBox cb = new CubicBox(5);
+		System.out.println(" CB volume is:" + cb.getVolume());
+	System.out.println(cb.toString());
+	}
+}
---
+++ src/main/java/monica/Operators.java
@@ -0,0 +1,75 @@
+package monica;
+
+public class Operators { public static void main(String[] args) {
+	int a= 12;
+	int b= 30;
+			
+			System.out.println(a+b);
+			System.out.println(a-b);
+			System.out.println(a*b);
+			System.out.println((double) a/b);
+            System.out.println(b%a);
+            System.out.println(++b);
+            System.out.println(b);
+            System.out.println(b--);
+            
+            b += a; // b=b+a
+            
+            System.out.println(b);
+            b -= 7;
+            System.out.println(b);
+            b *= 7;
+            System.out.println(b); 
+            b /= 3;
+            System.out.println(b);
+            b %= 10;
+            System.out.println(b);
+
+if (a == b ) {
+	System.out.println(" a equals b ");
+}
+if (a != b ) {
+	System.out.println(" a not equals b");
+}
+if ( a>b ) {
+System.out.println("a is bigger than b");
+}
+if (a<b) {
+	System.out.println("a is less than b");
+}
+if (a <= b) {
+	System.out.println("a is less or equal b");
+}
+
+int x,  y, z;
+x = y = z =100;
+if (x ==100 && y== 100 && z==100) {
+	System.out.println(" as expected ");
+}
+ int k = x == y ? 10 : 20;
+ int g;
+	 if ( x == y ) {
+	 g = 10;
+	 } else {
+		 g = 20;
+	 }			 
+
+	 System.out.println("k is " +k+ "and g is" +g);
+	 
+	 for (int i = 0 ; i < 10; i++ ) {
+		 for (int j =i; j <10; j++ ) {
+			 System.out.print(',');
+		 }
+		 System.out.println();
+		 }
+	
+	 
+	 
+
+}
+	 
+ }
+
+
+
+
