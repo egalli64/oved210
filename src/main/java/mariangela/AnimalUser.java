a6e7eeb66bf0257edbeb9a7b9ebbc64d962ddf39
Mariangela <mariangeladeluca@icloud.com>
2019-02-27 17:09:38 +0100

    gerarchie

---
+++ src/main/java/mariangela/AnimalUser.java
@@ -0,0 +1,16 @@
+package mariangela;
+
+public class AnimalUser {
+	public static void main(String[] args) {
+
+		Animal[] animals = new Animal[2];
+
+		animals[0] = new Dog("Fido", 1400);
+		animals[1] = new Cat("Silvestro", 800);
+
+		System.out.println(animals[0]);
+		System.out.println(animals[1].toString());
+
+	}
+
+}
