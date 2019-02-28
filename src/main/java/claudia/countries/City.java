3c1430a70fa149570baa746899a73d27a86a12d9
claudia <c.lategana@live.it>
2019-02-28 17:40:47 +0100

    other example

--- src/main/java/claudia/countries/City.java
+++ src/main/java/claudia/countries/City.java
@@ -13,6 +13,32 @@ public class City {
 		this.CityName = CityName;
 }
 
+	
+	
+	public String getCountryId() {
+		return CountryId;
+	}
+
+
+
+	public void setCountryId(String countryId) {
+		CountryId = countryId;
+	}
+
+
+
+	public String getCityName() {
+		return CityName;
+	}
+
+
+
+	public void setCityName(String cityName) {
+		CityName = cityName;
+	}
+
+
+
 	@Override
 	public String toString() {
 		return "City [CountryId=" + CountryId + ", CityName=" + CityName + "]";
