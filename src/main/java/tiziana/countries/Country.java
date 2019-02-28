45a90a6fe93745d3011d226c6ba8000dfbb574a6
Tiziana <tizianaleogrande@gmail.com>
2019-02-28 17:56:19 +0100

    ultimo salvataggio

---
+++ src/main/java/tiziana/countries/Country.java
@@ -0,0 +1,35 @@
+package tiziana.countries;
+
+public class Country {
+	private String id;
+	private String name;
+	
+	public Country(String id, String name) {
+		this.id=id;
+		this.name=name;
+		
+}
+
+	public String getId() {
+		return id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Country [id=" + id + ", name=" + name + "]";
+	}
+
+
+
+
+	
+	
+}
