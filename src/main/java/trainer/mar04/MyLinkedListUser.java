2bf4c79f300f200732b66f98846d0db79f578cfc
Manny <egalli64@gmail.com>
2019-03-04 17:44:11 +0100

    addLast for linked list

--- src/main/java/trainer/mar04/MyLinkedListUser.java
+++ src/main/java/trainer/mar04/MyLinkedListUser.java
@@ -3,13 +3,15 @@ package trainer.mar04;
 public class MyLinkedListUser {
 	public static void main(String[] args) {
 		MyLinkedList mll = new MyLinkedList();
-		
+				
 		System.out.println(mll.toString());
-		mll.addFirst(12);
+		mll.addFirst(21);
 		System.out.println(mll.toString());
-		mll.addFirst(3);
+		mll.addLast(12);
 		System.out.println(mll.toString());
-		mll.addFirst(42);
+		mll.addLast(3);
+		System.out.println(mll.toString());
+		mll.addLast(42);
 		System.out.println(mll.toString());
 	}
 }
