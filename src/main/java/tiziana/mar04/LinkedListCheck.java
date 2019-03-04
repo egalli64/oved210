29da824b5c37cdfb5db8bf7ffea8d7ecd57312ec
Tiziana <tizianaleogrande@gmail.com>
2019-03-04 17:48:32 +0100

    ultime classi

---
+++ src/main/java/tiziana/mar04/LinkedListCheck.java
@@ -0,0 +1,21 @@
+package tiziana.mar04;
+
+import java.util.LinkedList;
+
+public class LinkedListCheck {
+	public static void doubleLinkedListCheck() {
+		LinkedList<Double> linkedList = new LinkedList<>();
+		linkedList.add(2.5);
+		linkedList.addFirst(3.4);
+		linkedList.addLast(5.6);
+
+		System.out.println(linkedList);
+		System.out.println(linkedList.peekFirst() + "," + linkedList.peekLast());
+	}
+
+	public static void main(String[] args) {
+		doubleLinkedListCheck();
+
+	}
+
+}
---
+++ src/main/java/tiziana/mar04/MyLinkedList.java
@@ -0,0 +1,72 @@
+package tiziana.mar04;
+
+public class MyLinkedList {
+	private Node head = null;
+	private Node tail = null;
+
+	public MyLinkedList() {
+
+	}
+
+	public void addFirst(int value) {
+		Node node = new Node(value);
+		node.setNext(head);
+		head = node;
+	}
+
+	public void addLast(int value) {
+		Node node = new Node(value);
+		if (tail == null) {
+			head = node;
+			tail = node;
+		}
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder result = new StringBuilder("[ ");
+		Node cur = head;
+		while (cur != null) {
+			result.append(cur.getValue());
+			result.append(" ");
+			cur = cur.getNext();
+		}
+		result.append("]");
+
+		return result.toString();
+	}
+}
+
+class Node {
+	private int value;
+	private Node next;
+
+	public Node(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [value=" + value + ", next=" + next + "]";
+	}
+
+	public void addLast(int value) {
+
+	}
+}
