fca2f8f8a72817b8b2f9d44c0c67db0d2aa6889d
Administrator <donatella.rotelli@email.it>
2019-03-04 17:21:08 +0100

    altri esempi

--- src/main/java/donatella/Animal.java
+++ src/main/java/donatella/Animal.java
@@ -1,20 +1,51 @@
 package donatella;
 
+
 public abstract class Animal {
-	
-	String name;
-	int weight;
-	
-	public Animal(String name, int weight) {
-		 this.name = name;
-		 this.weight= weight;
-		 
+	public enum Fur {
+		SHORT, MEDIUM, LONG
+	};
+
+	protected int weight;
+	protected String name;
+	protected Fur fur;
+
+	public Animal(int weight, String name, Fur fur) {
+		if (weight <= 0) {
+			throw new IllegalArgumentException("Negative weight not accepted!");
+		}
+
+		this.weight = weight;
+		this.name = name;
+		this.fur = fur;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Fur getFur() {
+		return fur;
+	}
+
+	public void setFur(Fur fur) {
+		this.fur = fur;
 	}
 
 	@Override
 	public String toString() {
-		return "Animal [name=" + name + ", weight=" + weight + "]";
+		return "Animal [weight=" + weight + ", name=" + name + ", fur=" + fur + "]";
 	}
-	
-
 }
