6150a5e6aaf29908142be8f30789da233fdc9a8d
Administrator <donatella.rotelli@email.it>
2019-03-05 15:01:08 +0100

    Esempi Test

--- src/test/java/donatella/mar04/MyLinkedListTest.java
+++ src/test/java/donatella/mar04/MyLinkedListTest.java
@@ -4,6 +4,12 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
+
+
+
+
+
 public class MyLinkedListTest {
 	
 	@Test
@@ -16,57 +22,70 @@ public class MyLinkedListTest {
 
 	@Test
 	public void addFirstSimple() {
-		final int VALUE=12;
+    final int VALUE=12;
 		
-		MyLinkedList mll= new MyLinkedList();
-		assertNull(mll.getHead());
-		
-		mll.addFirst(VALUE);
-		
-		Node myHead=mll.getHead();
+	MyLinkedList mll = new MyLinkedList();
+	mll.addFirst(VALUE);
+
+	Node myHead = mll.getHead();
+	assertNotNull(myHead);
+	assertEquals(VALUE, myHead.getValue());
+
+	Node myTail = mll.getTail();
+	assertNotNull(myTail);
+	assertEquals(VALUE, myTail.getValue());
+}
+
+	@Test
+	public void addFirstCouple() {
+		final int FIRST_VALUE = 18;
+		final int SECOND_VALUE = 21;
+
+		MyLinkedList mll = new MyLinkedList();
+		mll.addFirst(FIRST_VALUE);
+		mll.addFirst(SECOND_VALUE);
+
+		Node myHead = mll.getHead();
 		assertNotNull(myHead);
-		
-		assertEquals(VALUE, myHead.getValue());
-		
-		}
+		assertEquals(SECOND_VALUE, myHead.getValue());
+
+		Node myTail = mll.getTail();
+		assertNotNull(myHead);
+		assertEquals(FIRST_VALUE, myTail.getValue());
+	}
+	@Test
+	public void addCoupleMixed() {
+		final int FIRST_VALUE = 18;
+		final int SECOND_VALUE = 21;
+
+		MyLinkedList mll = new MyLinkedList();
+		mll.addFirst(FIRST_VALUE);
+		mll.addLast(SECOND_VALUE);
+
+		Node myHead = mll.getHead();
+		assertNotNull(myHead);
+		assertEquals(FIRST_VALUE, myHead.getValue());
+
+		Node myTail = mll.getTail();
+		assertNotNull(myHead);
+		assertEquals(SECOND_VALUE, myTail.getValue());
+	}
 
 	@Test
 	public void addLastSimple() {
     final int VALUE=42;
 		
-		MyLinkedList mll= new MyLinkedList();
-		assertNull(mll.getHead());
-		
-		mll.addLast(VALUE);
-		
-		Node myHead=mll.getHead();
-		assertNotNull(myHead);
-		
-		assertEquals(VALUE, myHead.getValue());
-		
-		}
+    MyLinkedList mll = new MyLinkedList();
+	mll.addLast(VALUE);
+
+	Node myHead = mll.getHead();
+	assertNotNull(myHead);
+	assertEquals(VALUE, myHead.getValue());
+}
 	
-    @Test
-      public void addMix() {
-	  final int VALUE=24;
-	  final int VALUE2=30;
-	  MyLinkedList mll= new MyLinkedList();
-	  
-	  mll.addFirst(VALUE);
-	  mll.addLast(VALUE2);
-	  
-	  Node myHead=mll.getHead();
-		assertNotNull(myHead);
-		assertEquals(VALUE, myHead.getValue());
-		
-		 Node myTail=mll.getTail();
-			assertNotNull(myTail);
-			assertEquals(VALUE2, myTail.getValue());
-	 
-			Node myHeadNext= myHead.getNext();
-			assertNotNull(myHeadNext);
-			assertEquals(VALUE2, myHeadNext.getValue());
-  }
+	
+	
+    
   
    
  
