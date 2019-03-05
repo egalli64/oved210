b9ac00128340e1e211c31a4f5dff0f5f5e8660ff
alessandra bartesaghi <alessandrabartesaghi96@hotmail.com>
2019-03-05 12:01:56 +0100

    Test 5/03

--- src/main/java/alessandraB/mar04/MyLinkedList.java
+++ src/main/java/alessandraB/mar04/MyLinkedList.java
@@ -8,45 +8,58 @@ public class MyLinkedList {// listalinkata di interi
 
 	}
 
-	public void addFirst(int value) {
-		Node node = new Node(value);
-        node.setNext(head); //la variabile next all'interno di nodo è "head", il successore del nodo sarà
-                            //quello che è l'head corrente
-        head = node; //l'indirizzo head diventa l'indirizzo del primo elemento nella mia lista
-        
+	/**
+	 * //for test only, return the list head //@return the object head --> viene
+	 * usata per la generazione automatica dell'annotazione relativa alla classe
+	 */
+
+	Node getHead() {
+		return this.head = head;
+
+	}
+	
+	Node getTail() {
+		return this.tail = tail;
+		
 	}
 
-	public void setHead(Node head) {
-		this.head = head;
+	public void addFirst(int value) {
+		Node node = new Node(value);
+		node.setNext(head); // la variabile next all'interno di nodo è "head", il successore del nodo sarà
+							// quello che è l'head corrente
+		head = node; // l'indirizzo head diventa l'indirizzo del primo elemento nella mia lista
+
 	}
 
 	public void addLast(int value) {
-	
+
 		Node node = new Node(value);
-		if(tail == null)  {
+		if (tail == null) {
 			head = node;
 			tail = node;
-			
+
 		} else {
 			tail.setNext(node);
 			tail = node;
 		}
-			
-		}
-		
-		 
+
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder result = new StringBuilder("[");
-	
+
 		Node cur = head;
-		while(cur != null) {
+		while (cur != null) {
 			result.append(cur.getValue());
 			result.append(" ");
 			cur = cur.getNext();
-			
-		} result.append("]");
+
+		}
+		result.append("]");
 		return result.toString();
+	}
+
 }
 
 class Node {
@@ -57,30 +70,20 @@ class Node {
 		this.value = value;
 	}
 
-
 	public int getValue() {
 		return value;
 	}
-
 
 	public void setValue(int value) {
 		this.value = value;
 	}
 
-
 	public Node getNext() {
 		return next;
 	}
-
 
 	public void setNext(Node next) {
 		this.next = next;
 	}
 
-
-	
-
-	}
-
-	
 }
