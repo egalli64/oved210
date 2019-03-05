26a2e696fd15dbc2baeb363946f311d83fc98ade
Administrator <donatella.rotelli@email.it>
2019-03-05 17:22:23 +0100

    Esempi test

---
+++ src/main/java/donatella/mar05/Problem.java
@@ -0,0 +1,41 @@
+package donatella.mar05;
+
+
+
+public class Problem {
+	
+//	in:[2,1,1] out:2
+//	in [-1027,12,-1027,12,42] out 42
+	
+	public static int findUnique(int[]data) {
+		return 0;
+	}
+	
+//	in:[2,1,1] out:2
+//	in:[-1027,12,-1027,12,42]out:42
+	
+	public static int findBigger(int[]data) {
+		return 0;
+		
+	}
+// in [2,1,1] out: true
+//	/in:[-1027,11,-1029,15,41]out:false
+	
+	public static boolean hasEven(int[]data) {
+		
+		
+		return false;
+	}
+
+	// in 2 out: true
+//		/in:-1027 out:false
+	
+	public static boolean isEven(int value) {
+		if(value %2 == 0) {
+			return true;
+			
+		
+	
+}
+		return false;
+}}
---
+++ src/test/java/donatella/mar05/ProblemTest.java
@@ -0,0 +1,16 @@
+package donatella.mar05;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProblemTest {
+
+	@Test
+	public void testIsEven() {
+		int[] data=new int[] {1,2,3,4,3,2,1};
+		assertTrue(Problem.isEven(4));
+		
+	}
+
+}
