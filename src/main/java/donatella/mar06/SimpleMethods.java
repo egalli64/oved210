97b7dd7b437c19415efcfabf5a6457953a12892c
Administrator <donatella.rotelli@email.it>
2019-03-06 17:09:14 +0100

    Test 06/03/2019

---
+++ src/main/java/donatella/mar06/SimpleMethods.java
@@ -0,0 +1,117 @@
+package donatella.mar06;
+
+public class SimpleMethods {
+
+//	input: char c
+//	output: true if is an uppercase character('A')
+//	    false otherwise('h')
+//	'6'
+	
+	public static boolean IsUpper(char c) {
+		if(c >='A' && c <= 'Z')	{
+			return true;
+	} return false;
+	
+} 
+
+//	input: char c
+//	output: true if c is an alphabetic character ('A','c')
+
+	public static boolean isAlpha(char c) {
+	if((c >='A'&& c <='Z')|| (c >='a'&& c <='z')) {
+    return true; }
+ 	return false;
+
+
+}
+// input: char c
+//	output: char converted to uppercase
+//	'x'--> 'X'
+	
+	
+	public static char toUpper(char c) {
+		if(c<'a' || c>'z') {
+			return c;
+					}
+		
+		return(char) (c +'A'-'a');
+		
+		}
+		
+		
+//		input: int[]data;
+//	    otput:smallest element
+	
+//	{1,2,5,-7} -> -7
+//	{MAX_VALUE, MAX_VALUE, MAX_VALUE} -> MAX_VALUE
+	
+	public static int smallest(int[]data) {
+		int currentMinumun= Integer.MAX_VALUE;
+		if(data==null || data.length==0) {
+		return Integer.MAX_VALUE;
+				}
+		for(int i=0; i< data.length;i++) {
+			if(data [i] < currentMinumun) {
+				currentMinumun=data[i];
+			}
+		}
+	
+	
+return currentMinumun;
+	}
+
+//	imput: int[]data
+//  output:smallest element
+//  {1,2,5,-7}->3
+//  null,{}->-1
+
+                   public static int firstSmallestIndex(int[]data) {
+                	   if(data==null||data.length ==0){
+                       return -1;
+                	   }  
+                	   int pos =0;
+                 
+                	for(int i = 1; i < data.length; i++) {
+					if(data [i] < data[pos]) {
+                	          pos= i ;
+	               
+
+                	   }
+                	}  
+					return pos;            	  
+                	   
+                   }    	
+
+				public static int lastSmallestIndex(int[] data) {
+					  if(data==null||data.length ==0){
+	                       return -1;
+	                	   }  
+	                	   int pos =0;
+	                 
+	                	for(int i = 1; i < data.length; i++) {
+						if(data [i] <= data[pos]) {
+	                	          pos= i ;
+					
+					
+				}
+                   
+	                	}
+						return pos;}
+
+
+// {1,2,3} 2-> true
+// {1,2,3}, 7 -> false
+
+	public static boolean find(int[] data, int target) {
+		for (int i = 0; i < data.length; i++) {
+			if (target == data[i]) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+} 
+			
+				
+
---
+++ src/test/java/donatella/mar06/SimpleMethodsTest.java
@@ -0,0 +1,126 @@
+package donatella.mar06;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+import org.junit.Test;
+
+
+
+public class SimpleMethodsTest {
+
+	@Test
+	public void IsUpperA() {
+		assertTrue(SimpleMethods.IsUpper('A'));
+	}
+		@Test
+		public void IsUpperLowerH() {
+			assertFalse(SimpleMethods.IsUpper('h'));
+	}
+		@Test
+		public void IsUpperNumber() {
+			assertFalse(SimpleMethods.IsUpper('7'));
+	}
+       @Test
+      public void IsAlpha() {
+	  assertTrue(SimpleMethods.isAlpha('a'));
+	  assertFalse(SimpleMethods.isAlpha('9'));
+}
+		@Test 
+		public void toUpper() {
+		assertThat(SimpleMethods.toUpper('A'), is('A'));
+}		
+		
+		@Test 
+		public void toUpperLowerX() {
+		assertThat(SimpleMethods.toUpper('x'), is('X'));
+}		
+
+		@Test 
+		public void toUpperNumber() {
+		assertThat(SimpleMethods.toUpper('9'), is('9'));
+}		
+              @Test
+              public void smallestPlain() {
+	          int[]data= {1,2,5,-7};
+	           assertThat(SimpleMethods.smallest(data), is(-7));
+	  
+ }
+              
+               @Test 
+               public void smallestNull() {
+	            int[]data= {};
+	            assertThat(SimpleMethods.smallest(data),is(Integer.MAX_VALUE));
+               }
+	
+	 
+               @Test
+	           public void smallestEmpty() {
+		       int[]data= {};
+		       assertThat(SimpleMethods.smallest(data), is(Integer.MAX_VALUE));
+	 }
+	 
+	 
+	            @Test
+	 
+	            public void firstSmallerIndexNulla() {
+		        int[] data={};
+		        assertThat(SimpleMethods.firstSmallestIndex(data), is(-1));
+		 
+	 }
+	            @Test
+	            public void FirstIndexEmpty() {
+	            	int[] data= {};
+	            	assertThat(SimpleMethods.firstSmallestIndex(data), is(-1));
+	            }
+	            
+	            @Test
+	            public void FirstIndexPlain() {
+	            	int[] data= {1,2,5,-7};
+	            	assertThat(SimpleMethods.firstSmallestIndex(data), is(3));
+	            }
+	        	
+	            @Test
+	        	public void lastSmallestIndexNull() {
+	        		int[] data = null;
+	        		assertThat(SimpleMethods.lastSmallestIndex(data), is(-1));
+	        	}
+
+	        	@Test
+	        	public void lastSmallestIndexEmpty() {
+	        		int[] data = {};
+	        		assertThat(SimpleMethods.lastSmallestIndex(data), is(-1));
+	        	}
+
+	        	@Test
+	        	public void lastSmallestIndexPlain() {
+	        		int[] data = { 1, 2, 5, -7 };
+	        		assertThat(SimpleMethods.lastSmallestIndex(data), is(3));
+	        	}
+	        	
+	        	@Test
+	        	public void lastSmallestIndexDouble() {
+	        		int[] data = { -7, 2, 5, -7 };
+	        		assertThat(SimpleMethods.lastSmallestIndex(data), is(3));
+	        	}
+
+	        @Test
+	        public void findPlain() {
+	        	int[] data = {1,2,3};
+	        	assertTrue(SimpleMethods.find(data,2));
+	        	assertFalse(SimpleMethods.find(data,7));
+	 
+	        }
+		
+		
+			
+
+             
+	         
+
+}
+
+
+
+
+
+
