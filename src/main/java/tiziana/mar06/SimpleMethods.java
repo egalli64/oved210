56fab3e9350213500535429df81cbfd90d8a168b
Tiziana <tizianaleogrande@gmail.com>
2019-03-08 08:44:30 +0100

    aggiornamenti in mar06

--- src/main/java/tiziana/mar06/SimpleMethods.java
+++ src/main/java/tiziana/mar06/SimpleMethods.java
@@ -141,16 +141,54 @@ public class SimpleMethods {
 	}
 
 	/**
-	 * "abba" -> true "abbab" -> false
+	 * Checks if a string is a palindrome If the passed string is null returne false
+	 * Otherwise return true only if the string is a palindrome
+	 *
+	 * For example: "abba" -> true "abbab" -> false
+	 * 
+	 * @param s the string to be checked if palindrome
+	 * @return false only if the input is a palindrome
 	 */
+
 	public static boolean isPalindrome(String s) {
-		return false;
+		if (s == null) {
+			return false;
+		}
+		if (s.length() < 2) {
+			return false;
+		}
+		int left = 0;
+		int right = s.length() - 1;
+
+//		for (int i = 0; i < s.length(); i++) { /// metodo charAT per una stringa
+//			System.out.println(s.charAt(i));
+//
+//		}
+
+		for (; left < right; left++, right--) {
+			if (s.charAt(left) != s.charAt(right)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/**
 	 * {1, 2, 3} -> {3, 2, 1}
+	 *
 	 */
-	public static void reverse(int[] data) {
+
+	public static int[] reverseReturn(int[] data) {
+		if (data == null || data.length < 2) {
+			return data;
+
+		}
+		int[] result = new int[data.length];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = data[data.length - 1 - i];
+
+		}
+		return result;
 
 	}
 }
