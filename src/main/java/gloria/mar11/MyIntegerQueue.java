15f46ada2ed8d2f10e4f9d4aa51dc68994bb4f96
Gloria <gloria.foga29@gmail.com>
2019-03-11 17:38:28 +0100

    17:38

---
+++ src/main/java/gloria/mar11/MyIntegerQueue.java
@@ -0,0 +1,28 @@
+package gloria.mar11;
+
+import java.util.LinkedList;
+
+public class MyIntegerQueue {
+	private LinkedList<Integer> data;
+	
+	LinkedList<Integer> getData() {
+		return data;
+	}
+
+	public MyIntegerQueue() {
+		data = new LinkedList<Integer>();
+	}
+
+	public void enqueue(Integer value) {
+
+	}
+
+	public Integer dequeue() {
+		return 0;
+	}
+
+	public boolean isEmpty() {
+		return true;
+	}
+
+}
---
+++ src/test/java/gloria/mar11/CollectionExamplesTest.java
@@ -0,0 +1,14 @@
+package gloria.mar11;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CollectionExamplesTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
