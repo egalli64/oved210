909e06059fb46169e5070fe26879619d05336c11
elmonda <gjekmarkaj.elmonda@gmail.com>
2019-03-11 14:02:28 +0100

    11/03/19

--- src/main/java/elmonda/mar08/ArrayListProblems.java
+++ src/main/java/elmonda/mar08/ArrayListProblems.java
@@ -1,9 +1,7 @@
 package elmonda.mar08;
 
 import java.util.ArrayList;
-
-import org.thymeleaf.standard.expression.EqualsExpression;
-import org.thymeleaf.standard.expression.EqualsNotEqualsExpression;
+import java.util.Arrays;
 
 public class ArrayListProblems {
 	/**
@@ -20,9 +18,9 @@ public class ArrayListProblems {
 	 */
 	public static ArrayList<Double> intersection(double[] left, double[] right) {
 		ArrayList<Double> results = new ArrayList<>();
-		
+
 		// soluzione Professore
-		
+
 //		for (double lValue : left) {
 //			for (double rValue : right) {
 //				if (lValue == rValue) {
@@ -31,20 +29,18 @@ public class ArrayListProblems {
 //				}
 //			}
 //		}
-		
-		for(int i = 0; i < left.length; i++) {
-			for(int j = 0; j < right.length; j++) {
+
+		for (int i = 0; i < left.length; i++) {
+			for (int j = 0; j < right.length; j++) {
 				if (left[i] == right[j]) {
 					results.add(right[j]);
 				}
+			}
 		}
-		}
-		
+
 		return results;
 	}
 
-
-	
 	/**
 	 * Remove duplicates
 	 * 
@@ -52,7 +48,7 @@ public class ArrayListProblems {
 	 * 
 	 * @param data a list of Strings
 	 */
-	
+
 	// soluzione Professore
 //	public static void removeDuplicates(ArrayList<String> data) {
 //		System.out.println();
@@ -65,35 +61,90 @@ public class ArrayListProblems {
 //	}
 
 	public static void removeDuplicates(ArrayList<String> data) {
-		for(int i = 0; i < data.size() - 1; i++) {
-			for(int j = i+1; j < data.size(); j++) {
-				
-				if(data.get(i) == data.get(j)) {
-					
+		for (int i = 0; i < data.size() - 1; i++) {
+			String s = data.get(i);
+			for (int j = data.size() - 1; j > i; j--) {
+				String t = data.get(j);
+				if (s.contentEquals(t)) {
+
 					data.remove(j);
 				}
 			}
-				
+
 		}
 	}
-	
+
 	/**
 	 * Given n-1 values in [1, n], return the missing value
 	 * 
 	 * for(int i = 1; i < 10; i++) {
+	 * 
 	 * <pre>
 	 * {1, 3} -> 2   // ho i numeri da 1  3 , però manca il 2  // dobbiamo sandire il nostro array e trovare quello che manca
 	 * {1, 3, 5, 7, 9, 8, 6, 2} -> 4   //
 	 * </pre>
 	 */
-	
-	// come valore gli ritorniamo solo il valore che manca
-	public static int missingValue(int[] data) {  
-	
-		return 0;
+
+//	// come valore gli ritorniamo solo il valore che manca
+//	public static int missingValue(int[] data) {  
+//	
+//		return 0;
+//	}
+
+	public static int missingValue(int[] data) {
+
+		for (int i = 1; i <= data.length + 1; i++) {
+			boolean found = false;
+
+			for (int j = 0; j < data.length; j++) {  // opp for(int value : data) { 
+				if (data[j] == i) {                 // opp if (value ==i) {
+					found = true;
+					break;
+				}
+			}
+
+			if (found == false) {         // opp if(!found) {
+				return i;                 // return i;
+			}
+		}
+		
+		return 0;             // throw new IllegalStateException;  // eccezioni check devo scriverle nelle classi public static int, in questo caso è uncheck
 	}
+	
+public static int missingValueBySorting(int[] data) {
+	
+	Arrays.sort(data);
+	for( int i = 0; i < data.length; i++) {   
+	                        // non dobbiamo lavorare sugli indici ma su data di i
+		if(data[i] !=(i+1)) {
+		//found = true;
+		//break;
+	
+	return i+1;  // return= i+1
+		}
+	}
+	return 0;
+	
+	
+	
 }
 
+// algoritmo lineare O(n)    1345->
+public static int missingValueLinear(int[] data) {     
+	boolean[] flags = new boolean[data.length + 1];
 
-
+	for (int i=0 ; i < data.length; i++) {
+		flags[data[i] -1] = true; 
+		   //valore corrente -1, in flags di 0 ci metto 1
+		  // in flags avremo true per gli elenti presenti in data
+	}
+		for (int i= 0; i < flags.length; i++) {
+			if(flags[i]== false) {
+				return i+1;
+			
+		}   // falso per gli elementi non presenti
+	}
+	return 0;
+}
+}  // se non trova niente tiro l'eccezione
 
--- src/test/java/elmonda/mar08/ArrayListProblemsTest.java
+++ src/test/java/elmonda/mar08/ArrayListProblemsTest.java
@@ -49,11 +49,55 @@ public class ArrayListProblemsTest {
 }
 		ArrayListProblems.removeDuplicates(data);  // non ritorna niente, riceve in data, lo manipola  ein data ci dev'essere il risultato,
 		// tutti i duplicati devono essere buttati via
-		assertThat(data.size(),is(1));        // voglio che data conenga un solo elemento
-		//assertEquals(1, data.size());  
-		
+		//assertThat(data.size(),is(1));        // voglio che data conenga un solo elemento
+		  
+		assertThat(data.size(), is(1));
+		//assertEquals(1, data.size());
 		assertThat(data.get(0), is("aa"));
-		assertEquals("aa", data.get(0));
+		//assertEquals("aa", data.get(0));
+	}
+	
+	@Test
+	public void missingValue() {
+		
+		int[] data = {1, 3, 4, 5};
+		
+		//int result = ArrayListProblems.missingValue(data);
+		int missing = ArrayListProblems.missingValue(data);
+		//assertEquals(2, result);
+		assertThat(missing, is(2));
+	}
+	
+	@Test
+	public void missingValue4in9() {
+		int[] data = {1, 3, 5, 7, 9, 8, 6, 2 };
+		
+		int missing = ArrayListProblems.missingValue(data);
+		assertThat(missing, is(4));
+	}
+	
+	@Test
+	public void missingValueSort2in3() {
+		int[] data = {1, 3};
+		
+		int missing = ArrayListProblems.missingValueBySorting(data);
+		assertThat(missing, is(2));
+	}
+	@Test
+	public void missingValueSort4in9() {
+		int[] data = {1, 3, 5, 7, 9, 8, 6, 2};
+		
+		int missing = ArrayListProblems.missingValueBySorting(data);
+		assertThat(missing, is(4));
+	}
+	
+	@Test
+	
+	public void missingValueLinear() {
+		int[] data = {1, 3, 5, 7, 9, 8, 6, 2};
+		
+		int missing = ArrayListProblems.missingValueLinear(data);
+		assertThat(missing, is(4));
 	}
 }
 
