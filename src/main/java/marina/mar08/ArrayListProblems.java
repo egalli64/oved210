1cd5256aa69c368edc7b0249b37bfedfa4924c0d
Marina <marinapaolino994@gmail.com>
2019-03-11 14:01:31 +0100

    Missing value 11/03

--- src/main/java/marina/mar08/ArrayListProblems.java
+++ src/main/java/marina/mar08/ArrayListProblems.java
@@ -1,6 +1,7 @@
 package marina.mar08;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ArrayListProblems {
 	/**
@@ -55,18 +56,73 @@ public class ArrayListProblems {
 	public static void removeDuplicates(ArrayList<String> data) {
 
 		for (int i = 0; i < data.size(); i++) {
-			for (int j = i + 1 ; j < data.size(); j++) {
+			for (int j = i + 1; j < data.size(); j++) {
 
 				if (data.get(i) == data.get(j)) {
-				 data.remove(j);
+					data.remove(j);
 				}
 			}
 		}
-		
-	}
-	
-	
-	
-	
 
-}
+	}
+
+	/**
+	 * Given n - 1 values in [i, n], return the missing value
+	 * 
+	 * <pre>
+	 * {1,3} -> 2
+	 * 
+	 * {1,3,5,7,9,8,6,2} -> 4
+	 * </pre>
+	 * 
+	 * *
+	 */
+
+	public static int missingValue(int[] data) {
+
+		for (int i = 1; i <= data.length + 1; i++) {
+			boolean found = false;
+			for (int value : data) {
+				if (value == i) {
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				return i;
+			}
+		}
+		return 0;
+		// throw new IllegalStateException();
+	}
+
+	public static int missingValueBySorting(int[] data) {
+
+		Arrays.sort(data);
+
+		for (int i = 0; i < data.length; i++) {
+			if (data[i] != i + 1) {
+				return i + 1;
+			}
+		}
+		return 0;
+	}
+
+	public static int missingValueLinear(int[] data) {
+
+		boolean[] flags = new boolean[data.length + 1];
+
+		for (int i = 0; i < data.length; i++) {
+			flags[data[i] - 1] = true;
+		}
+		
+		for (int i = 0; i < flags.length; i++) {
+			if (flags[i] == false) {
+				return i + 1;
+			}
+
+		}
+		return 0;
+	}
+
+} 
