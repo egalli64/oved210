c2b6441131ee2d18d60b88f82fb5eb0d36f3deb2
Sara <farinato10@gmail.com>
2019-03-12 17:36:36 +0100

    new

---
+++ src/test/java/sara/mar12/HashExercisesTest.java
@@ -0,0 +1,29 @@
+package sara.mar12;
+
+import static org.hamcrest.core.Is.*;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.junit.Test;
+
+public class HashExercisesTest {
+	
+	@Test
+	public void inserctionPlain() {
+		HashSet<Integer> left = new HashSet<Integer>(Arrays.asList(1, 2, 3 ));
+		HashSet<Integer> right = new HashSet<Integer>(Arrays.asList(4, 5, 6 ));
+
+		HashSet<Integer> results = HashExercises.intersection(left, right);
+		assertThat(results.size(), is (2));
+		assertTrue(results.contains(2));
+		assertTrue(results.contains(3));
+		}
+	
+	@Test
+	public void inserctionEmpty() {
+		HashSet<Integer> left = new HashSet<Integer>(Arrays.asList(1, 2, 3 ));
+		HashSet<Integer> right = new HashSet<Integer>(Arrays.asList( 2, 3, 4 ));
+	}
+}
