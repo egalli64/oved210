1f258f1e1e091a5c3d840aa4f026620e20daa5d6
Sara <farinato10@gmail.com>
2019-03-12 15:12:47 +0100

    new

--- src/test/java/sara/mar05/ProblemTest.java
+++ src/test/java/sara/mar05/ProblemTest.java
@@ -2,6 +2,7 @@ package sara.mar05;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
 public class ProblemTest {
@@ -12,7 +13,42 @@ public class ProblemTest {
 		assertEquals(2, Problem.findUniqueN2(data));	
 	}
 	
+	@Test
+	public void findUniqueSort() {
+		int[] data = new int [] {2, 1, 1};
+		assertEquals(2, Problem.findUniqueSort(data));	
+	}
 	
+	
+//	@Test
+//	public void findBiggerPlain() {
+//		int[] data = new int [] {2, 1, 1};
+//		assertEquals(2, Problem.findBigger(data));	
+//	}
+//	@Test
+//	public void testHasEven() {
+//		int[] data = new int [] {2, 1, 1};
+//		assertTrue(Problem.hasEven(data));	
+//	}
+//	@Test
+//	public void testHasEvenFalse() {
+//		int[] data = new int [] {2, 1, 1};
+//		assertFalse(Problem.hasEven(data));	
+//	}
+//	@Test
+//	public void isEvenTrue() {
+//		assertTrue(Problem.isEven(2));	
+//	}
+//	@Test
+//	public void isEvenFalse() {
+//		assertFalse(Problem.isEven(-1));	
+//	}
+	
+	@Test
+	public void findUniqueHash() {
+		int[] data = new int [] {2, 1, 1};
+		assertEquals(2, Problem.findUniqueHash(data));	
+	}
 	
 
 }
