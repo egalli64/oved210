b4426a92e43eb69108d9d51a5913a113a143bc1d
Manny <egalli64@gmail.com>
2019-03-12 17:37:21 +0100

    difference setup problem

--- src/main/java/trainer/mar12/HashExercises.java
+++ src/main/java/trainer/mar12/HashExercises.java
@@ -9,15 +9,39 @@ public class HashExercises {
 	 * <pre>
 	 * {1, 2, 3}, {2, 3, 4} -> {2, 3}
 	 * {1, 2, 3}, {4, 5, 6} -> {}
-	 * <pre>
+	 * </pre>
 	 * 
-	 * @param left a set
+	 * @param left  a set
 	 * @param right another set
 	 * @return intersection between left and right
 	 */
 	public static HashSet<Integer> intersection(HashSet<Integer> left, HashSet<Integer> right) {
 		HashSet<Integer> result = new HashSet<>();
-		
+
+		for (Integer value : left) {
+			if (right.contains(value)) {
+				result.add(value);
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * Difference of two sets
+	 * 
+	 * <pre>
+	 * {1, 2, 3}, {2, 3, 4} -> {1}
+	 * {1, 2, 3}, {4, 5, 6} -> {1, 2, 3}
+	 * </pre>
+	 * 
+	 * @param left  a set
+	 * @param right another set
+	 * @return difference between left and right
+	 */
+	public static HashSet<Integer> difference(HashSet<Integer> left, HashSet<Integer> right) {
+		HashSet<Integer> result = new HashSet<>();
+
 		return result;
 	}
 }
