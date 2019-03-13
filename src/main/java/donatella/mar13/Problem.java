bd138587a8a027ba402dce7003d31a4c0cdf9724
Administrator <donatella.rotelli@email.it>
2019-03-13 17:46:38 +0100

    treeMap examples

---
+++ src/main/java/donatella/mar13/Problem.java
@@ -0,0 +1,30 @@
+package donatella.mar13;
+
+public class Problem {
+	
+//	{2,2,2,1,}-> 2;
+//	{2,2,2,1,1,3,3,1,1}->1
+	
+//	@param data an array of integer
+//	@return the most popular value in data
+	
+	
+	
+	public static int mostPopular(int[]data) {
+		return 0;
+
+}
+//	@param data
+//	@param value
+//	@return the number of values in data
+	
+	public static int count(int[]data, int value) {
+		for(int i=0; i<data.length; i++) {
+			if(data[i]==value);
+			
+		}
+		
+		return value;
+	
+	}
+}
