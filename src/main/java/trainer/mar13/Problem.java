21a418e5061006e0d3b238cf1e6c49a440df4a71
Manny <egalli64@gmail.com>
2019-03-13 17:22:47 +0100

    most popular problem

---
+++ src/main/java/trainer/mar13/Problem.java
@@ -0,0 +1,15 @@
+package trainer.mar13;
+
+public class Problem {
+	/**
+	 * <pre>
+	 * {2, 2, 2, 1, 1} -> 2
+	 * {2, 2, 2, 1, 1, 3, 3, 1, 1} -> 1
+	 * </pre>
+	 * @param data an array of integer
+	 * @return the most popular value in data
+	 */
+	public static int mostPopular(int[] data) {
+		return 0;
+	}
+}
