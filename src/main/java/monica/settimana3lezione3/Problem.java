e6887916b9dee2df96bf63b04217434724c937f8
Monica <monica.conti93@gmail.com>
2019-03-13 17:46:21 +0100

    13/03/19

---
+++ src/main/java/monica/settimana3lezione3/Problem.java
@@ -0,0 +1,38 @@
+package monica.settimana3lezione3;
+
+public class Problem {
+
+	/**
+	 * {2,2,2,1,1}  -> 2
+	 * {2,2,2,1,1,3,3,1,1} -> 1 
+	 * 
+	 * @param data an array of integer
+	 * @return return the most popular value in data
+	 */
+	
+	
+	
+	public static int count(int []data, int value) {
+		int x = 0;
+		for (int i = 0; i<data.length; i++ ) {
+			if (data[i]== value){
+//			x = ;
+				
+			}
+		}
+		
+		
+		
+		return 0;
+	}
+	
+	
+	
+	public static int mostPopular (int []data) {
+		
+		
+		
+		return 0;
+	}
+	
+}
---
+++ src/test/java/monica/settimana3lezione3/ProblemTest.java
@@ -0,0 +1,17 @@
+package monica.settimana3lezione3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProblemTest {
+
+//	@Test
+//	public void count() {
+//		int [] value = {2,2,2,1,1};
+//		
+//		assertThat(Problem.count((  data, value) , 2));
+//		
+//	}
+
+}
