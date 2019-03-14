a1f6dc6a7387b6854df6d567f07cd7de1f72e481
Marina <marinapaolino994@gmail.com>
2019-03-14 17:36:25 +0100

    Esercizi 14/04

--- src/main/java/marina/mar13/Problem.java
+++ src/main/java/marina/mar13/Problem.java
@@ -1,6 +1,30 @@
 package marina.mar13;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem {
+
+	/**
+	 * 
+	 * @param data
+	 * @param value
+	 * @return the number of value in data
+	 */
+
+	public static int count(int[] data, int value) {
+
+		int result = 0;
+
+		for (int i = 0; i < data.length; i++) {
+			if (data[i] == value) {
+				result++;
+			}
+		}
+		return result;
+	}
+
 	/**
 	 * <pre>
 	 * {2, 2, 2, 1, 1} -> 2
@@ -12,29 +36,83 @@ public class Problem {
 	 * @return the most popular value in data
 	 */
 
-	
 	public static int mostPopular(int[] data) {
-		return 0;
+
+		if (data == null || data.length == 0) {
+			throw new IllegalArgumentException("Bad input data");
+		}
+
+		int result = 0;
+		int resultFreq = 0;
+
+//		int result = data[0];
+//		int resultCount = count(data,result);
+
+		for (int i = 1; i < data.length; i++) {
+			int currentFreq = count(data, data[i]);
+			if (currentFreq > resultFreq) {
+				result = data[i];
+				resultFreq = currentFreq;
+			}
+		}
+
+		return result;
 	}
 
+	public static int mostPopularSorted(int[] data) {
 
-	
-	/**
+		if (data == null || data.length == 0) {
+			throw new IllegalArgumentException("Bad input data");
+		}
 
-	 * @param data
-	 * @param value
-	 * @return the number of value in data
-	 */
-	
-	public static int count (int[] data, int value) {
+		Arrays.sort(data);
 
+		int result = data[0];
+		int resultFreq = 1;
+		int current = result;
+		int currentFreq = 1;
 
-//		for (int i = 0; i< data.length; i++) {
-//			if ( data[i] == value ) {
-//				return value;
+		for (int i = 1; i < data.length; i++) {
+			if (data[i] != current) {
+				if (currentFreq > resultFreq) {
+					result = current;
+					resultFreq = currentFreq;
+				}
+				current = data[i];
+				currentFreq = 1;
+			} else {
+				currentFreq++;
+			}
+		}
+		if (resultFreq > currentFreq) {
+			return result;
+		} else {
+			return current;
+		}
+	}
+
+//	public static int mostPopularHash(int[] data) {
+//		
+//		if (data == null || data.length == 0) {
+//			throw new IllegalArgumentException("Bad input data");
+//		}
+//	
+//		Map <Integer, Integer> counters = new HashMap<>();
+//		
+//		for (int i=0; i < data.length; i++) {
+//			if(counters.containsKey(data[i])) {
+//				int value = counters.get(data[i]);
+//				counters.put(data[i], value + 1);
 //			}
-//	}
-	return 0;
-	}
+//		}
+//		
+////		for (int key: data) {
+////			Integer counter = counters.get(key);
+////		if (counter == null) {
+////			counter = 0;
+////		}
+////		counters.put(key, counter + 1);
+////	}
+////
+////	}
 }
-
