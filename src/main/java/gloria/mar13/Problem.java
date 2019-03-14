835f916e4f125e705776030baacd8a8743aee512
Gloria <gloria.foga29@gmail.com>
2019-03-14 17:44:13 +0100

    14/03, 17:44

--- src/main/java/gloria/mar13/Problem.java
+++ src/main/java/gloria/mar13/Problem.java
@@ -1,5 +1,9 @@
 package gloria.mar13;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem {
 
 	/**
@@ -18,10 +22,10 @@ public class Problem {
 			throw new IllegalArgumentException("");
 		}
 
-		int result = data[0];
-		int resultCount = count(data, result);
+		int result = data[0]; // int result = 0;
+		int resultCount = count(data, result); // int resultCount = 0;
 
-		for (int i = 1; i < data.length; i++) {
+		for (int i = 1; i < data.length; i++) { // for( int i = 0; ... ; ...){
 			int popular = count(data, data[i]);
 			if (popular > resultCount) {
 				result = data[i];
@@ -64,4 +68,65 @@ public class Problem {
 	 * @return the number of values in data
 	 */
 
+	public static int mostPopularArraysSort(int[] data) {
+		if (data == null || data.length == 0) {
+			throw new IllegalArgumentException("");
+		}
+
+		Arrays.sort(data);
+
+		int result = data[0];
+		int current = result;
+		int frequencyResult = 1;
+		int frequencyCurrent = 1;
+		for (int i = 1; i < data.length; i++) {
+			if (data[i] != current) {
+				if (frequencyCurrent > frequencyResult) {
+					result = current;
+					frequencyResult = frequencyCurrent;
+				}
+
+				current = data[i];
+				frequencyCurrent = 1;
+			} else {
+				frequencyCurrent++;
+			}
+		}
+
+		if (frequencyResult > frequencyCurrent) {
+			return result;
+		} else {
+			return current;
+
+		}
+	}
+
+	public static int mostPopularHash(int[] data) {
+		if (data == null || data.length == 0) {
+			throw new IllegalArgumentException("Bad input data");
+		}
+
+		Map<Integer, Integer> counters = new HashMap<>();
+
+		for (int key : data) { // for(int i = 0; i < data.lenght; i++)
+			Integer counter = counters.get(key);
+			if (counter == null) { // if(counters.containsKey(data[i])); int value = counters.get(data[i]);
+									// counters.put(data[i], value +1)}else{counters.put(data[i], 1);
+				counter = 0;
+			}
+			counters.put(key, counter + 1);
+		}
+		
+		int result = 0;
+		int frequency = 0;
+		for (Map.Entry<Integer, Integer> entry : counters.entrySet()) {
+			int currentFrequency = entry.getValue();
+			if (currentFrequency > frequency) {
+				result = entry.getKey();
+				frequency = entry.getValue();
+			}
+		}
+		return result;
+
+	}
 }
--- src/test/java/gloria/mar13/ProblemTest.java
+++ src/test/java/gloria/mar13/ProblemTest.java
@@ -1,7 +1,6 @@
 package gloria.mar13;
 
 import org.junit.Test;
-
 import static org.hamcrest.core.Is.*;
 import static org.junit.Assert.*;
 
@@ -12,17 +11,52 @@ public class ProblemTest {
 		int[] data = { 2, 2, 2, 1, 1 };
 		assertThat(Problem.count(data, 2), is(3));
 	}
-	
-	@Test 
+
+	@Test
 	public void countTwo() {
-		int[] data = {2, 2, 2, 1, 1, 3, 3, 1, 1};
+		int[] data = { 2, 2, 2, 1, 1, 3, 3, 1, 1 };
 		assertThat(Problem.count(data, 1), is(4));
 	}
-	
+
 	@Test
 	public void mostPopular() {
-		int[] data = {2, 2, 2, 1, 1};
+		int[] data = { 2, 2, 2, 1, 1 };
 		assertThat(Problem.mostPopular(data), is(2));
+	}
+
+	@Test
+	public void mostPopularSecond() {
+		int[] data = { 2, 2, 2, 1, 1, 3, 3, 1, 1 };
+		assertThat(Problem.mostPopular(data), is(1));
+	}
+
+	@Test
+	public void mostPopularSort() {
+		int[] data = { 2, 2, 2, 1, 1, 3, 3, 1, 1 };
+		assertThat(Problem.mostPopularArraysSort(data), is(1));
+	}
+
+	@Test
+	public void mostPopularHash() {
+		Problem problem = new Problem();
+	}
+
+	@Test
+	public void countFirst() {
+		int data[] = { 2, 2, 2, 1, 1 };
+		assertThat(Problem.count(data, 2), is(3));
+	}
+
+	@Test
+	public void countOther() {
+		int data[] = { 2, 2, 2, 1, 1, 3, 3, 1, 1 };
+		assertThat(Problem.count(data, 3), is(2));
+	}
+
+	@Test
+	public void countMissing() {
+		int data[] = { 2, 2, 2, 1, 1, 3, 3, 1, 1 };
+		assertThat(Problem.count(data, 4), is(0));
 	}
 
 }
