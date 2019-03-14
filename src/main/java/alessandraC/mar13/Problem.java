fbf7ec0bc1a7e3707200bc5760b0f1c4177061a5
Alessandra <alessandra.cirone@katamail.com>
2019-03-14 17:42:13 +0100

    save 2 - 14 mar

--- src/main/java/alessandraC/mar13/Problem.java
+++ src/main/java/alessandraC/mar13/Problem.java
@@ -1,18 +1,12 @@
 package alessandraC.mar13;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem {
 
-	/**
-	 * <pre>
-	 * {2, 2, 2, 1, 1}, 2 -> 3
-	 * {2, 2, 2, 1, 1, 3, 3, 1, 1}, 3 -> 2
-	 * {2, 2, 2, 1, 1, 3, 3, 1, 1}
-	 * </pre>
-	 * 
-	 * @param data an array of Integer
-	 * @return the most popular value in data
-	 *
-	 */
+	private static Object key;
 
 	/**
 	 * 
@@ -26,11 +20,106 @@ public class Problem {
 		int result = 0;
 
 		for (int i = 0; i < data.length; i++) {
-			if (data [i] == value) {
+			if (data[i] == value) {
 				result += 1;
 			}
 		}
 
 		return result;
 	}
+
+	/**
+	 * <pre>
+	 * {2, 2, 2, 1, 1} -> 2
+	 * {2, 2, 2, 1, 1, 3, 3, 1, 1} -> 1
+	 * </pre>
+	 * 
+	 * @param data an array of Integer
+	 * @return the most popular value in data
+	 *
+	 */
+	public static int mostPopular(int[] data) {
+		if (data == null || data.length == 0) {
+			throw new IllegalArgumentException("Bad input data");
+		}
+		int result = data[0];
+		int resultCount = count(data, result);
+
+		for (int i = 1; i < data.length; i++) {
+			int CurCount = count(data, data[i]);
+			if (CurCount > resultCount) {
+				result = data[i];
+				resultCount = CurCount;
+			}
+		}
+
+		return result;
+
+	}
+
+	public static int mostPopularSorted(int[] data) {
+		if (data == null || data.length == 0) {
+			throw new IllegalArgumentException("Bad input data");
+		}
+
+		Arrays.sort(data);
+
+		int result = data[0];
+		int resultFrequency = 1;
+		int candidate = result;
+		int candidateFrequency = 1;
+
+		for (int i = 1; i < data.length; i++) {
+			if (data[i] != candidate) {
+				if (candidateFrequency > resultFrequency) {
+					result = candidate;
+					resultFrequency = candidateFrequency;
+
+				}
+			}
+			if (resultFrequency < candidateFrequency) {
+				return result;
+
+			} else {
+
+			}
+
+		}
+		return candidate;
+
+	}
+
+	public static int mostPopularHash(int[] data) {
+		if(data == null || data.length == 0) {
+			throw new IllegalArgumentException("Bad input data");
+			
+		}
+		
+		int result = 0;
+		int frequency = 0;
+		int candidate = result;
+		int candidateFrequency = 1;
+		
+		
+		Map<Integer, Integer> counters = new HashMap<>();
+		
+			for(int i = 0; i < data.length; i++) {
+			Integer counter = counters.get(key);
+			if(counter == null) {
+				counter = 0;
+			}
+//			counters.put(data[i], value + 1);
+			
+				
+			}
+		
+	
+		
+	
+		
+		
+		return 0;
+		
+	}
+
 }
--- src/test/java/alessandraC/mar13/ProblemTest.java
+++ src/test/java/alessandraC/mar13/ProblemTest.java
@@ -18,4 +18,38 @@ public class ProblemTest {
 		int[] data = { 2, 2, 2, 1, 1 };
 		assertThat(Problem.count(data, 2), is(3));
 	}
+
+	@Test
+	public void mostPopularValue() {
+		int[] data = { 2, 2, 2, 1, 1 };
+		assertThat(Problem.mostPopular(data), is(2));
+
+	}
+
+	@Test
+	public void mostPopularValue2() {
+		int[] data = { 2, 2, 2, 1, 1, 3, 3, 1, 1 };
+		assertThat(Problem.mostPopular(data), is(1));
+
+	}
+
+	@Test
+	public void mostPopularSorted() {
+		int[] data = { 2, 2, 2, 1, 1 };
+		assertThat(Problem.mostPopularSorted(data), is(2));
+
+	}
+
+	@Test
+	public void mostPopularSorted2() {
+		int[] data = { 2, 2, 2, 1, 1, 3, 3, 1, 1 };
+		assertThat(Problem.mostPopularSorted(data), is(1));
+	}
+	@Test
+	public void get() {
+		int [] data = {2, 2, 2, 1, 1};
+		assertThat(Problem.mostPopularHash(data), is(2));
+	}
+//	@Test
+//	public void 
 }
