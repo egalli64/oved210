e3066628b8d81b16ab07a1aedb1f67f1b3b04f38
claudia <c.lategana@live.it>
2019-03-14 17:41:24 +0100

    problem test su hashmap

--- src/main/java/claudia/mar13/Problem.java
+++ src/main/java/claudia/mar13/Problem.java
@@ -1,5 +1,9 @@
 package claudia.mar13;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem {
 	/**
 	 * <pre>
@@ -11,17 +15,84 @@ public class Problem {
 	 * @return the most popular value in data
 	 */
 	public static int mostPopular(int[] data) {
-		return 0;
+		int result = data[0];
+		int resultCount = counter(data, result);
+		for (int i = 1; i < data.length; i++) {
+			int frequency = counter(data, data[i]);
+
+			if (frequency > resultCount) {
+				result = data[i];
+			}
+		}
+
+		return result;
 	}
 
 	public static int counter(int[] data, int value) {
-		int x = 0;
+		int counter = 0;
 		for (int i = 0; i < data.length; i++) {
 			if (data[i] == value) {
-				return x;
+				counter++;
 			}
-
 		}
-		return 0;
+		return counter;
+	}
+
+	public static int mostPopularSorted(int[] data) {
+		if (data == null || data.length == 0) {
+			throw new IllegalArgumentException("Bad input data");
+		}
+		Arrays.sort(data);
+
+		int result = data[0];
+		int frequency = 1;
+		int current = result;
+		int currentFrequency = 1;
+
+		for (int i = 1; i < data.length; i++) {
+			if (data[i] != current) {
+				if (currentFrequency > result) {
+					result = current;
+					frequency = currentFrequency;
+				}
+				current = data[i];
+				currentFrequency = 1;
+			} else {
+				currentFrequency++;
+			}
+		}
+		if (frequency > currentFrequency) {
+			return result;
+		} else {
+			return current;
+		}
+
+	}
+
+	public static int mostPopularHash(int[] data) {
+		if (data == null || data.length == 0) {
+			throw new IllegalArgumentException("Bad input data");
+		}
+		Map<Integer, Integer> counters = new HashMap<>();
+
+		for (int i = 0; i < data.length; i++) {
+			if (counters.containsKey(data[i])) {
+				int value = counters.get(data[i]);
+				counters.put(data[i], value + 1);
+
+			} else {
+				counters.put(data[i], 1);
+			}
+		}
+		int result = 0;
+		int frequency = 0;
+		for (Map.Entry<Integer, Integer> entry : counters.entrySet()) {
+			int currentFrequency = entry.getValue();
+			if (currentFrequency > frequency) {
+				result = entry.getKey();
+				frequency = entry.getValue();
+			}
+		}
+		return result;
 	}
 }
--- src/test/java/claudia/mar13/ProblemTest.java
+++ src/test/java/claudia/mar13/ProblemTest.java
@@ -7,11 +7,44 @@ import static org.hamcrest.core.Is.*;
 
 public class ProblemTest {
 
-//	@Test
-//	public void testCounter() {
-//		int[]data = {2, 2, 2, 1, 1};
-//		assertThat(Problem.counter(data, value), is (2));
-		
+	@Test
+	public void testCounter() {
+		int[] data = { 2, 2, 2, 1, 1 };
+		assertThat(Problem.counter(data, 1), is(2));
+
 	}
 
+	@Test
+	public void testCounter2() {
+		int[] data = { 2, 2, 2, 1, 1 };
+		assertThat(Problem.counter(data, 2), is(3));
+	}
+
+	@Test
+	public void testCounter3() {
+		int[] data = { 2, 2, 2, 1, 1 };
+		assertThat(Problem.counter(data, 4), is(0));
+	}
+
+	@Test
+	public void testMostPopular() {
+		int[] data = { 2, 2, 2, 1, 1 };
+		assertThat(Problem.mostPopular(data), is(2));
+	}
+	@Test
+	public void testMostPopular2() {
+		int[] data = { 2, 2, 2, 1, 1, 3, 3 };
+		assertThat(Problem.mostPopular(data), is(2));
+}
+	@Test
+	public void testMostPopularSorted() {
+		int[] data = { 1, 1, 1, 3, 5, 5, 5, 5, 7};
+		assertThat(Problem.mostPopularSorted(data), is(5));
+	}
+	@Test
+	public void testMostPopularHash() {
+		int[] data = { 1, 1, 1, 3, 5, 5, 5, 5, 7};
+		assertThat(Problem.mostPopularHash(data), is(5));
+	}
+}
 
