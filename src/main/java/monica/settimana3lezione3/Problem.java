da76d5b79e73f572c9d4b085021250fe45495471
Monica <monica.conti93@gmail.com>
2019-03-14 17:43:24 +0100

    14/03/19

--- src/main/java/monica/settimana3lezione3/Problem.java
+++ src/main/java/monica/settimana3lezione3/Problem.java
@@ -1,38 +1,110 @@
 package monica.settimana3lezione3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem {
 
 	/**
-	 * {2,2,2,1,1}  -> 2
-	 * {2,2,2,1,1,3,3,1,1} -> 1 
+	 * {2,2,2,1,1} -> 2 {2,2,2,1,1,3,3,1,1} -> 1
 	 * 
 	 * @param data an array of integer
 	 * @return return the most popular value in data
 	 */
-	
-	
-	
-	public static int count(int []data, int value) {
-		int x = 0;
-		for (int i = 0; i<data.length; i++ ) {
-			if (data[i]== value){
-//			x = ;
-				
+
+	public static int count(int[] data, int value) {
+		int counter = 0;
+		for (int i = 0; i < data.length; i++) {
+			if (data[i] == value) {
+				counter = counter + 1; // equivalente a : counter ++ oppure counter += 1
+
 			}
 		}
-		
-		
-		
-		return 0;
+		return counter;
 	}
-	
-	
-	
-	public static int mostPopular (int []data) {
-		
-		
-		
-		return 0;
+
+	public static int mostPopular(int[] data) {
+		int result = data[0];
+		int resultFrequency = Problem.count(data, result);
+
+		for (int i = 0; i < data.length; i++) {
+			int frequency = Problem.count(data, data[i]);
+
+			if (frequency > resultFrequency) {
+				result = data[i];
+				resultFrequency = frequency;
+			}
+
+		}
+		return result;
 	}
-	
+// esempio difficile
+
+	public static int mostPopular2(int[] data) {
+
+		Arrays.sort(data);
+
+		int result = data[0];
+		int resultFrequency = 1;
+		int current = result;
+		int currentFrequency = 1;
+		for (int i = 1; i < data.length; i++) {
+			if (data[i] == data[i - 1]) {
+				currentFrequency++;
+			}
+			if (data[i] != data[i - 1]) {
+				current = data[i];
+				currentFrequency = 1;
+
+				if (currentFrequency > resultFrequency) {
+					result = current;
+					resultFrequency = currentFrequency;
+				}
+
+				current = data[i];
+				currentFrequency = 1;
+			} else {
+				currentFrequency++;
+			}
+
+		}
+		if (resultFrequency > currentFrequency) {
+			return result;
+
+		} else {
+			return current;
+		}
+
+	}
+
+//altro problema
+
+	public static int mostPopularHash(int[] data) {
+
+		Map<Integer, Integer> counters = new HashMap<>();
+
+		for (int i = 0; i < data.length; i++) {
+			if (counters.containsKey(data[i])) {
+				int value = counters.get(data[i]);
+				counters.put(data[i], value + 1);
+			} else {
+				counters.put(data[i], 1);
+			}
+			int result = 0;
+			int frequency = 0;
+			for (Map.Entry<Integer, Integer> entry : counters.entrySet()) {
+
+				int currentFrequency = entry.getValue();
+				if (currentFrequency > frequency) {
+					result = entry.getKey();
+					frequency = entry.getValue();
+				}
+			}
+		
+			
+		}
+	return 0;
+	}
 }
--- src/test/java/monica/settimana3lezione3/ProblemTest.java
+++ src/test/java/monica/settimana3lezione3/ProblemTest.java
@@ -1,17 +1,52 @@
 package monica.settimana3lezione3;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.core.Is.*;
 import org.junit.Test;
 
 public class ProblemTest {
 
-//	@Test
-//	public void count() {
-//		int [] value = {2,2,2,1,1};
-//		
-//		assertThat(Problem.count((  data, value) , 2));
-//		
-//	}
+	@Test
+	public void count() {
+		int[] data = { 2, 2, 2, 1, 1 };
+		assertThat(Problem.count(data, 1), is(2));
+	}
+	
+	
+	@Test
+	public void countA() {
+		int[] data = { 2, 2, 2, 1, 1 };
+		assertThat(Problem.count(data, 2), is(3));
+	}
+	
+	@Test 
+	public void countB() {
+		int[] data = {2,2,2,1,1,3,3,1,1};
+		assertThat(Problem.count(data, 3), is(2));
+	}
+	
+	@Test
+	public void countC() {
+		int []data = {2,2,2,1,1,3,3,1,1};
+		assertThat(Problem.count(data, 4), is(0));
+	}
+	@Test
+	public void mostPopularA() {
+		int []data = { 2, 2, 2, 1, 1 };
+		assertThat(Problem.mostPopular(data), is(2));
+	}
+	
+	@Test
+	public void mostPopular2() {
+		int []data = { 2, 2, 2, 1, 1, 3, 5, 5 };
+		assertThat(Problem.mostPopular2(data), is(2));
+		}
+
+	@Test
+	public void mostPopularHas() {
+		int []data = { 2, 2, 2, 1, 1, 3, 5, 5 };
+		assertThat(Problem.mostPopularHash(data), is(2));
+
+	}
 
 }
