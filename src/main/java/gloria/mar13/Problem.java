9c45a689b263adeeb098f7e90486345f5fb9657d
Gloria <gloria.foga29@gmail.com>
2019-03-14 13:05:46 +0100

    14/03

--- src/main/java/gloria/mar13/Problem.java
+++ src/main/java/gloria/mar13/Problem.java
@@ -14,12 +14,22 @@ public class Problem {
 	 */
 
 	public static int mostPopular(int[] data) {
-		for (int i = 0; i < data.length; i++) {
-			for (int j = 0; j < data.length; j++) {
+		if (data == null || data.length == 0) {
+			throw new IllegalArgumentException("");
+		}
 
+		int result = data[0];
+		int resultCount = count(data, result);
+
+		for (int i = 1; i < data.length; i++) {
+			int popular = count(data, data[i]);
+			if (popular > resultCount) {
+				result = data[i];
+				resultCount = popular;
 			}
 		}
-		return 0;
+
+		return result;
 	}
 
 	/**
@@ -31,12 +41,27 @@ public class Problem {
 	 */
 
 	public static int count(int[] data, int value) {
-		int result = 0;
+		int count = 0;
+
 		for (int k = 0; k < data.length; k++) {
 			if (data[k] == value) {
-				return result;
+				count += 1;
 			}
 		}
-		return 0;
+
+		return count;
 	}
+
+	/**
+	 * <pre>
+	 * {2, 2, 2, 1, 1},2 --> 3;
+	 * {2, 2, 2, 1, 1, 3 ,3, 1, 1}, 3 --> 2;
+	 * {2, 2, 2, 1, 1, 3 ,3, 1, 1}, 4 --> 0;
+	 * </pre>
+	 * 
+	 * @param data  an array of integers (possibly) w/repeti
+	 * @param value an integer that could be in data
+	 * @return the number of values in data
+	 */
+
 }
--- src/test/java/gloria/mar13/ProblemTest.java
+++ src/test/java/gloria/mar13/ProblemTest.java
@@ -2,24 +2,27 @@ package gloria.mar13;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.Is.*;
 import static org.junit.Assert.*;
 
-import org.hamcrest.core.Is;
-
 public class ProblemTest {
+
+	@Test
+	public void count() {
+		int[] data = { 2, 2, 2, 1, 1 };
+		assertThat(Problem.count(data, 2), is(3));
+	}
 	
-//	@Test
-//	public void countIn() {
-//		int [] data = {2, 2, 2, 1, 1};
-//		
-//		assertThat(countIn.contains(), is(3));
-//		assertTrue();
-//		assertTrue();
-//		assertTrue();
-//		
-		
-		
-//	}
+	@Test 
+	public void countTwo() {
+		int[] data = {2, 2, 2, 1, 1, 3, 3, 1, 1};
+		assertThat(Problem.count(data, 1), is(4));
+	}
+	
+	@Test
+	public void mostPopular() {
+		int[] data = {2, 2, 2, 1, 1};
+		assertThat(Problem.mostPopular(data), is(2));
+	}
 
 }
