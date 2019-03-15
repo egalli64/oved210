81231dea011da67ec87f8dec35ff73ab5d5dc366
Marina <marinapaolino994@gmail.com>
2019-03-15 17:40:15 +0100

    Exercises 15/03

--- src/main/java/marina/mar15/Problems.java
+++ src/main/java/marina/mar15/Problems.java
@@ -1,13 +1,18 @@
 package marina.mar15;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+
+import org.springframework.web.multipart.support.StringMultipartFileEditor;
+import org.thymeleaf.templateresolver.StringTemplateResolver;
 
 public class Problems {
 
 	/**
 	 * <pre>
-	 * {1, 1, 1, 4, 5, 6, 4, 6, 5} -> {1, 4, 6, 5
+	 * {1, 1, 1, 4, 5, 6, 4, 6, 5} -> {1, 4, 6, 5}
 	 * {} -> {}
 	 * null -> exception ? {}
 	 * </pre>
@@ -17,16 +22,133 @@ public class Problems {
 	 * @return a List containing only unique elements from data
 	 * 
 	 */
-	
-	
-	public static List <Integer> killDuplicates(ArrayList<Integer> data) {
-		
-		
-		
-		
-		return data;
-		
+
+	public static List<Integer> killDuplicates(List<Integer> data) {
+
+		if (data == null || data.size() == 0) {
+			throw new IllegalArgumentException("Bad input data");
+		}
+
+		ArrayList<Integer> buffer = new ArrayList<Integer>(data);
+		buffer.sort(null);
+
+		ArrayList<Integer> result = new ArrayList<Integer>();
+
+		Integer previous = buffer.get(0);
+		result.add(previous);
+		for (int i = 1; i < buffer.size(); i++) {
+			Integer current = buffer.get(i);
+			if (current != previous) {
+				result.add(current);
+				previous = current;
+			}
+		}
+		return result;
 	}
+
+	public static List<Integer> killDuplicatesHash(List<Integer> data) {
+
+		if (data == null) {
+			throw new IllegalArgumentException("Bad input data");
+		}
+
+		HashSet<Integer> hash = new HashSet<>(data);
+		ArrayList<Integer> result = new ArrayList<Integer>(hash);
+
+		return result;
+
+		// return new ArrayList<>(new HashSet<>(data));
+
+	}
+
+	/**
+	 * 
+	 * If value is odd, return "Weird" If value is even and in [6 .. 20], return
+	 * "Weird"
+	 * 
+	 * If value is even and in [2 .. 5], return "Not Weird" If value is even and
+	 * greater than 20, return "Not Weird"
+	 * 
+	 * @param value an integer
+	 * @return "Weird" or "Not weird"
+	 */
+
+	public static String ifElse(int value) {
+
+		if (value < 1) {
+			throw new IllegalAccessError("Bad input data");
+		}
+
+		if (value % 2 == 1 || (value < 21 && value > 5)) {
+			return "Weird";
+		} else {
+			return "Not Weird";
+		}
+	}
+
+	/**
+	 * 2 -> {2, 4, 6, 8, 10, 12, 14, 18, 20}
+	 * 
+	 * @param value an integer
+	 * @return the first ten multiples of value
+	 */
+
+	public static int[] multiples(int value) {
+
+		int[] results = new int[10];
+
+		for (int i = 0; i < results.length; i++) {
+
+			results[i] = value * (i + 1);
+		}
+
+// Oppure:
+//		
+//		for (int i = 1; i < results.length; i++) {
+//
+//			results[i-1] = value * i;
+//		}
+
+		return results;
+	}
+
+	/**
+	 * <pre>
+	 * cat, tac -> true
+	 * catt, tacc -> false
+	 * </pre>
+	 * 
+	 * 
+	 * @param left  a string
+	 * @param right another string
+	 * @return true if left is an anagram of right
+	 */
+
+	static public boolean isAnagram(String left, String right) {
+
+		if (right.length() == 0 || left.length() == 0 && right.length() != left.length()) {
+			return false;
+		}
+	
+		char[] newleft = left.toCharArray();
+		char[] newright = right.toCharArray();
+
+		Arrays.sort(newleft);
+		Arrays.sort(newright);
+
+		for (int i = 0; i < newright.length; i++) {
+			if (newright[i] != newleft[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 	
--- src/test/java/marina/mar15/ProblemsTest.java
+++ src/test/java/marina/mar15/ProblemsTest.java
@@ -1,20 +1,150 @@
 package marina.mar15;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.junit.Test;
 
 public class ProblemsTest {
 
 	@Test
 	public void testKillDuplicates() {
-		
+
+		List<Integer> array = Arrays.asList(1, 1, 1, 4, 5, 6, 4, 6, 5);
+		List<Integer> result = Problems.killDuplicates(array);
+		assertThat(result.size(), is(4));
+		assertThat(result.get(0), is(1));
+		assertThat(result.get(1), is(4));
+		assertThat(result.get(2), is(5));
+		assertThat(result.get(3), is(6));
 	}
 
 	@Test
 	public void testKillDuplicatesEmpty() {
-		
+		List<Integer> array = Arrays.asList();
+		try {
+			Problems.killDuplicates(array);
+			fail("ifElse() should throw an exception");
+		} catch (IllegalArgumentException ex) {
+			// as expected
+		}
 	}
-	
-	
+
+	@Test
+	public void testKillDuplicatesHash() {
+
+		List<Integer> array = Arrays.asList(1, 1, 1, 4, 5, 6, 4, 6, 5);
+		List<Integer> result = Problems.killDuplicatesHash(array);
+		assertThat(result.size(), is(4));
+		assertThat(result.get(0), is(1));
+		assertThat(result.get(1), is(4));
+		assertThat(result.get(2), is(5));
+		assertThat(result.get(3), is(6));
+	}
+
+	@Test
+	public void testKillDuplicatesHashEmpty() {
+		List<Integer> array = Arrays.asList();
+		List<Integer> result = Problems.killDuplicatesHash(array);
+		assertThat(result.size(), is(0));
+	}
+
+	@Test
+	public void testifElseOdd() {
+
+		assertThat(Problems.ifElse(1), is("Weird"));
+	}
+
+	@Test
+	public void testifElseEven20() {
+
+		assertThat(Problems.ifElse(20), is("Weird"));
+	}
+
+	@Test
+	public void testifElseEven() {
+
+		assertThat(Problems.ifElse(2), is("Not Weird"));
+	}
+
+	@Test
+	public void testifElse0() {
+
+		try {
+			Problems.ifElse(0);
+			fail("ifElse() should throw an exception");
+		} catch (IllegalAccessError ex) {
+			// as expected
+		}
+	}
+
+	@Test
+	public void testMultiplesTwo() {
+
+		int[] results = Problems.multiples(2);
+		assertThat(results.length, is(10));
+		assertThat(results[0], is(2));
+		assertThat(results[1], is(4));
+		assertThat(results[2], is(6));
+		assertThat(results[3], is(8));
+		assertThat(results[4], is(10));
+		assertThat(results[5], is(12));
+		assertThat(results[6], is(14));
+		assertThat(results[7], is(16));
+		assertThat(results[8], is(18));
+		assertThat(results[9], is(20));
+	}
+
+	@Test
+	public void testMultiplesThree() {
+
+		int[] results = Problems.multiples(3);
+		assertThat(results.length, is(10));
+		assertThat(results[0], is(3));
+		assertThat(results[1], is(6));
+		assertThat(results[2], is(9));
+		assertThat(results[3], is(12));
+		assertThat(results[4], is(15));
+		assertThat(results[5], is(18));
+		assertThat(results[6], is(21));
+		assertThat(results[7], is(24));
+		assertThat(results[8], is(27));
+		assertThat(results[9], is(30));
+
+	}
+
+	@Test
+	public void testisAnagram() {
+		String right = "cat";
+		String left = "tac";
+		assertTrue(Problems.isAnagram(right, left));
+
+	}
+
+	@Test
+	public void testisAnagram2() {
+		String right = "cat";
+		String left = "act";
+		assertTrue(Problems.isAnagram(right, left));
+
+	}
+
+	@Test
+	public void testisAnagram3() {
+		String right = "catt";
+		String left = "tacc";
+		assertFalse(Problems.isAnagram(right, left));
+	}
+
+	@Test
+	public void testisAnagramEmpty() {
+		String right = "";
+		String left = "";
+		assertFalse(Problems.isAnagram(right, left));
+
+	}
+
 }
