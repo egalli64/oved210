30634bd48f228a509af232162026abaf9602bf31
Monica <monica.conti93@gmail.com>
2019-03-15 11:43:22 +0100

    15/03/19

--- src/main/java/monica/settimana3lezione5/Problems.java
+++ src/main/java/monica/settimana3lezione5/Problems.java
@@ -1,4 +1,4 @@
-package monica.settimana3lezione5;  // 15/03/19
+package monica.settimana3lezione5; // 15/03/19
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -7,35 +7,40 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-
 /**
- * {1,1,1,4,5,6,4,6,5} -> {1,4,6,5}
- * {} -> {}
- * null -> exception? {}?
- * {1,1,1,1,1,1,1,1,1,1}  -> {1}
- * data un array list of integer
- * a list containing only unique elements from data //elimina i duplicati e mantiene i valori
+ * {1,1,1,4,5,6,4,6,5} -> {1,4,6,5} {} -> {} null -> exception? {}?
+ * {1,1,1,1,1,1,1,1,1,1} -> {1} data un array list of integer a list containing
+ * only unique elements from data //elimina i duplicati e mantiene i valori
  * 
  *
  */
 
-
-
 public class Problems {
 
-	public static List<Integer> killDuplicates (List<Integer> data) {
-	
-		ArrayList <Integer > buffer = new ArrayList<Integer> (data); // in questo modo non modifico i dati originali
-		buffer.sort(null);                                           //Collections.sort(data);
-		        
-		
-		
-		
-		
-		
-		throw new UnsupportedOperationException("not yet implmentes");
+	public static List<Integer> killDuplicates(List<Integer> data) {
+		if (data == null || data.isEmpty()) {
+
+			throw new UnsupportedOperationException("not yet implmentes");
+		}
+
+		ArrayList<Integer> buffer = new ArrayList<Integer>(data); // in questo modo non modifico i dati originali
+		buffer.sort(null); // Collections.sort(data); //passo null per avere l'ordine in modo naturale
+
+		ArrayList<Integer> result = new ArrayList<Integer>();
+
+		Integer value = buffer.get(0);
+		result.add(value);
+
+		for (int i = 1; i < buffer.size(); i++) {
+			int current = buffer.get(i);       // cosi gli dico di lavorare sull'elemento corrente
+			if (current != value) {           // confrontami l'elemento corrente con quello precedente
+				result.add(current);
+				value = current;
+			}
+
+		}
+		return result;
+
 	}
-	
-	
-	
+
 }
