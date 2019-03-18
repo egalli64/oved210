e6d8f1dfeeccac959fa06316cdcd703412b8e3a1
Manny <egalli64@gmail.com>
2019-03-18 16:17:19 +0100

    A few details more

--- src/main/java/trainer/mar18/User.java
+++ src/main/java/trainer/mar18/User.java
@@ -1,5 +1,7 @@
 package trainer.mar18;
 
+import java.util.HashMap;
+
 public class User {
 	public static void main(String[] args) {
 		System.out.println("base:");
@@ -7,23 +9,30 @@ public class User {
 		b.k();
 
 		System.out.println("derived:");
-		Derived d = new Derived();		
+		Derived d = new Derived();
 		d.k();
-				
-		Base[] array = { b, d, new Derived2()};
-		for(Base base: array) {
+
+		Base[] array = { b, d, new Derived2() };
+		for (Base base : array) {
 			base.k();
 		}
 
-		for(int i = 0; i < array.length; i++) {
+		for (int i = 0; i < array.length; i++) {
 			array[i].k();
 		}
-		
-		Happy[] happyArray = { new Derived(), new DerivedAnother2()};
-		for(Happy happy: happyArray) {
-			happy.smile();
-		}
-	
+
 		Another a = new DerivedAnother2();
+		System.out.println(a instanceof DerivedAnother2);
+
+		Happy[] happyArray = { new Derived(), new DerivedAnother2() };
+		for (Happy happy : happyArray) {
+			happy.smile();
+
+			// if is a Derived, call its k()
+			if (happy instanceof Derived) {
+				Derived derived = (Derived) happy;
+				derived.k();
+			}
+		}
 	}
 }
