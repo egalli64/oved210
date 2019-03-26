3bd984d241b55f5588d83d2d6e750e7533853e5c
claudia <c.lategana@live.it>
2019-03-26 17:47:48 +0100

    plsql

---
+++ Scripts/Claudia/plsqlMar26Function.sql
@@ -0,0 +1,37 @@
+--pl/sql procedure, function, trigger
+
+--creare una procedura per incrementare il salario di alcuni dipendenti
+create procedure update_coder_salary(
+    p_coder_id in coders.coder_id%type,
+    p_factor in number
+    ) as 
+    v_count integer;
+    begin
+    select count(rowid)
+    into v_count
+    from coders
+    where coder_id = p_coder_id;
+    
+    if v_count = 1 then
+        update coders
+        set salary = salary + (salary * p_factor)
+        where coder_id = p_coder_id;
+        commit;
+    end if;
+    
+    exception
+    when others then
+    ROLLBACK;
+        
+    end update_coder_salary;
+    
+    call update_coder_salary(103,1);
+    
+    call update_coder_salary(p_coder_id => 104, p_factor => 1.5);
+    
+    begin
+    update_coder_salary(105, 3);
+    end;
+    
+    
+    
