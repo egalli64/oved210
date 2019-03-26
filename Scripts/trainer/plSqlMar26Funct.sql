f1c830b185fdae87b6568b73fd0ac5e077d425ac
Manny <egalli64@gmail.com>
2019-03-26 17:22:35 +0100

    pl/sql functions

--- Scripts/trainer/plSqlMar26Funct.sql
+++ Scripts/trainer/plSqlMar26Funct.sql
@@ -5,3 +5,26 @@
 -- grant create procedure to oved;
 -- grant create trigger to oved;
 
+create or replace procedure update_coder_salary(
+    p_coder_id in coders.coder_id%type,
+    p_factor in number
+) as
+    v_count integer;
+begin
+    select count(rowid)
+    into v_count
+    from coders
+    where coder_id = p_coder_id;
+    
+    if v_count = 1 then
+        update coders
+        set salary = salary + (salary * p_factor)
+        where coder_id = p_coder_id;
+        commit;
+    end if;
+exception
+    when others then
+        rollback;
+end update_coder_salary;
+/    
+
