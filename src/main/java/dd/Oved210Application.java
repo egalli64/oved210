724623cbeb7972160a31c6f7cbba9bc14b87cbc5
Alessandra <alessandra.cirone@katamail.com>
2019-03-28 17:44:09 +0100

    save 28mar

--- src/main/java/dd/Oved210Application.java
+++ src/main/java/dd/Oved210Application.java
@@ -1,13 +1,29 @@
 package dd;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Oved210Application {
-
+	private static final Logger log = LoggerFactory.getLogger(Oved210Application.class);
+	
 	public static void main(String[] args) {
+		log.info("I'm just entered the main method");
 		SpringApplication.run(Oved210Application.class, args);
 	}
+	
+	@Bean
+	public CommandLineRunner simple () {
+		return (args) -> {
+			log.trace("I'm in simple() now!");
+			
+		};
+		
+	};
+	
 
 }
