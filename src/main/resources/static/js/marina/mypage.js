5b22915d971752f8cbc1578d4b6403d39588559c
Marina <marinapaolino994@gmail.com>
2019-04-03 15:29:59 +0200

    03/04

---
+++ src/main/resources/static/js/marina/mypage.js
@@ -0,0 +1,11 @@
+alert("Welcome to La casa de papel fan page!");
+
+
+$('button').click(function() {
+	<a href="http://linknull">here</a>;
+});
+
+$('#nice').click(function() {
+	$(this).val("Nice Choice!");
+
+});
