b34f17d77ddfcd506262a4245101386917bc4631
Sara <farinato10@gmail.com>
2019-04-04 17:45:23 +0200

    new

--- src/main/resources/static/js/sara/hello.js
+++ src/main/resources/static/js/sara/hello.js
@@ -1,21 +1,19 @@
 
+let name = prompt("Welcome", "User");
 
-let name = prompt("welcome", "user");
-	
-	function generateMessage(user, date) {
-		return "Hello " + user + ". You connected at " + date;
-	}
-	
-	$(function() {
-	$('#header').slideDown(1000);	
-	$('#header2').slideDown(3000);	
+function generateMessage(user, date) {
+	return "Hello " + user + ". You connected at " + date;
+}
+
+$(function() {
+	$('#header').slideDown(1000);
+	$('#header2').slideDown(2000);
 	$('#signature').text("SeaHotel Inc. 1978");
-	$('#colorSwapper').click(function(){
+	$('#colorSwapper').click(function() {
 		$('.changeColor').toggleClass('blueColor');
 	});
-	
-	});
+});
 
 
-	<p class="changeColor" id="signature">&copy; SeaHotel Inc. 1978</p>
+<p class="changeColor" id="signature">&copy; SeaHotel Inc. 1978</p>
 	
