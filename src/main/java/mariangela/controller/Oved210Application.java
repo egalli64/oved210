fc929a6e752b43d75b9976ebaa8adbd7b4635426
Mariangela <mariangeladeluca@icloud.com>
2019-04-05 17:36:16 +0200

    HTML 05/04

---
+++ src/main/java/mariangela/controller/Oved210Application.java
@@ -0,0 +1,16 @@
+package mariangela.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oved210Application {
+	private static final Logger log = LoggerFactory.getLogger(Oved210Application.class);
+
+	public static void main(String[] args) {
+		log.trace("Starting up the Oved app");
+		SpringApplication.run(Oved210Application.class, args);
+	}
+}
