4823943e83bef147b199c018cdb57fcb0ccda5e6
Manny <egalli64@gmail.com>
2019-04-05 16:06:29 +0200

    th: more stuff

--- src/main/java/trainer/apr/OvedController.java
+++ src/main/java/trainer/apr/OvedController.java
@@ -1,15 +1,28 @@
 package trainer.apr;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import trainer.jdbcSpring.Region;
 
 @Controller
 public class OvedController {
 	private static final Logger logger = LoggerFactory.getLogger(OvedController.class);
+
+	@Value("${admin}")
+	private String admin;
+
+	@Value("${admin.password}")
+	private String adminPassword;
 
 	@PostMapping("/trainer/login")
 	public String login( //
@@ -17,8 +30,27 @@ public class OvedController {
 			@RequestParam(name = "password") String password, //
 			Model model) {
 		logger.debug("Login attempt for user: " + user);
-
 		model.addAttribute("user", user);
-		return "trainer/welcome";
+
+		if (user.equals(admin) && password.equals(adminPassword)) {
+			return "trainer/welcome";
+		}
+
+		logger.info("No login for " + user + "/" + password);
+		return "trainer/rejected";
+	}
+
+	@GetMapping("/trainer/simple")
+	public String simple(Model model) {
+		logger.trace("Enter simple()");
+
+		// JPA emulation
+		List<Region> regions = Arrays.asList(
+			new Region(1, "Europe"),
+			new Region(2, "Antartica")
+		);
+
+		model.addAttribute("regions", regions);
+		return "trainer/simple";
 	}
 }
