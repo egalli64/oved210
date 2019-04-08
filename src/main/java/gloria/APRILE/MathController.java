41cb781baff3c2991a3555906234fb5cee0e79b3
Gloria <gloria.foga29@gmail.com>
2019-04-08 17:18:35 +0200

    02/04

---
+++ src/main/java/gloria/APRILE/MathController.java
@@ -0,0 +1,27 @@
+package gloria.APRILE;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MathController {
+	private static final Logger log = LoggerFactory.getLogger(Oved210Controller.class);
+	
+	@GetMapping("/gloria/add")
+	public String  add( //
+			@RequestParam int x, //
+			@RequestParam int y, //
+			Model model) { //
+		log.trace("add:" + x + "," + y);
+				
+		model.addAttribute("x", x);
+		model.addAttribute("y", y);
+		model.addAttribute("result", x + y);
+		
+		return "/gloria/result";
+	}
+}
