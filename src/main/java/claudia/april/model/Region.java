e6d28f4c2380cf02e5dd59cc19f8b54f2b685527
claudia <c.lategana@live.it>
2019-04-08 17:41:41 +0200

    collegamento sql

---
+++ src/main/java/claudia/april/model/Region.java
@@ -0,0 +1,38 @@
+package claudia.april.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="REGIONS")
+public class Region {
+	@Id
+	private long regionId;
+	private String regionName;
+	
+	public Region() {
+		
+	}
+	
+	public long getRegionId() {
+		return regionId;
+	}
+	public void setRegionId(long regionId) {
+		this.regionId = regionId;
+	}
+	public String getRegionName() {
+		return regionName;
+	}
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Region [regionId=" + regionId + ", regionName=" + regionName + "]";
+	}
+	
+
+}
