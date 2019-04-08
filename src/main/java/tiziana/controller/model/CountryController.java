f5adb471eef10479f84ea4811b1b5e7dcb76c04d
Tiziana <tizianaleogrande@gmail.com>
2019-04-08 17:45:26 +0200

    save new

---
+++ src/main/java/tiziana/controller/model/CountryController.java
@@ -0,0 +1,27 @@
+package tiziana.controller.model;
+
+	import org.slf4j.Logger;
+	import org.slf4j.LoggerFactory;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.stereotype.Controller;
+	import org.springframework.ui.Model;
+	import org.springframework.web.bind.annotation.GetMapping;
+
+	@Controller
+	public class CountryController {
+		private static final Logger log = LoggerFactory.getLogger(CountryController.class);
+		
+		@Autowired
+		private CountryRepository repo;
+		
+		@GetMapping("tiziana/countries")
+		public String allCountriess(Model model) {
+			log.trace("get all countries");
+			model.addAttribute("countries", repo.findAll());
+			return "tiziana/countries";
+			
+		}
+
+	}
+
+	
---
+++ src/main/java/tiziana/controller/model/CountryRepository.java
@@ -0,0 +1,14 @@
+package tiziana.controller.model;
+
+	import org.springframework.data.repository.CrudRepository;
+	import org.springframework.stereotype.Repository;
+
+
+	@Repository
+	public interface CountryRepository 
+		extends CrudRepository<Country, Long> {
+
+	}
+
+
+
