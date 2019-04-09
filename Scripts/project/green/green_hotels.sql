d67518b58f3ae9f6e8420ef3debbf75fc1f11504
Francesca <francescapoerio93@libero.it>
2019-04-09 15:23:31 +0200

    new

---
+++ Scripts/project/green/green_hotels.sql
@@ -0,0 +1,45 @@
+DROP TABLE  green_hotels;
+
+ create table green_hotels (
+hotel_id number (6) primary key,
+hotel_name varchar2 (40),
+city varchar2 (30),
+room_counter number (6) 
+);
+
+insert into green_hotels  (hotel_id, hotel_name, city, room_counter)
+values(1 , 'Hotel Tulipano', 'Milano', 10);
+
+insert into green_hotels  (hotel_id, hotel_name, city, room_counter)
+values(2 , 'Hotel Margherita', 'Roma', 10);
+
+insert into green_hotels  (hotel_id, hotel_name, city, room_counter)
+values(3 , 'Hotel Girasole', 'Firenze', 7);
+
+insert into green_hotels  (hotel_id, hotel_name, city, room_counter)
+values(4 , 'Hotel Giglio', 'Napoli', 5);
+
+insert into green_hotels  (hotel_id, hotel_name, city, room_counter)
+values(5, 'Hotel Orchidea', 'Venezia', 5);
+
+insert into green_hotels  (hotel_id, hotel_name, city, room_counter)
+values(6 , 'Hotel Rosa', 'Milano', 10);
+
+insert into green_hotels  (hotel_id, hotel_name, city, room_counter)
+values(7 , 'Hotel Fresia', 'Roma', 10);
+
+insert into green_hotels  (hotel_id, hotel_name, city, room_counter)
+values(8 , 'Hotel Semiramide', 'Firenze', 7);
+
+insert into green_hotels  (hotel_id, hotel_name, city, room_counter)
+values(9 , 'Hotel Spezia', 'Napoli', 5);
+
+insert into green_hotels  (hotel_id, hotel_name, city, room_counter)
+values(10, 'Hotel Imperial', 'Venezia', 5);
+
+insert into green_hotels  (hotel_id, hotel_name, city, room_counter)
+values(11 , 'Hotel Excelsior', 'Napoli', 5);
+
+insert into green_hotels  (hotel_id, hotel_name, city, room_counter)
+values(12, 'Hotel Clinton', 'Venezia', 5);
+
---
+++ src/main/java/francesca/Aprile/model/Region.java
@@ -0,0 +1,30 @@
+package francesca.Aprile.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Regions")
+public class Region {
+	@Id
+	private long regionId;
+	private String regionName;
+	public long getRegionId() {
+		return regionId;
+	}
+	public void setRegionId(long regionId) {
+		this.regionId = regionId;
+	}
+	public String getRegionName() {
+		return regionName;
+	}
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+	@Override
+	public String toString() {
+		return "Region [regionId=" + regionId + ", regionName=" + regionName + "]";
+	}
+}
+
