ab78d80739a2e4724d1a7a0b80cc19c9a2fdd8af
Tiziana <tizianaleogrande@gmail.com>
2019-04-10 09:42:33 +0200

    save

--- src/main/java/project/red/ClientController.java
+++ src/main/java/project/red/ClientController.java
@@ -1,5 +1,23 @@
 package project.red;
 
-public class ClientController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class ClientController {
+	private static final Logger log = LoggerFactory.getLogger(ClientController.class);
+
+	@Autowired
+	private ClientRepository repo;
+
+	@GetMapping("/project/red/clients")
+	public String allClients(Model model) {
+		log.trace("get all clients");
+		model.addAttribute("clients", repo.findAll());
+		return "/project/red/clients";
+	}
 }
--- src/main/java/project/red/ClientRepository.java
+++ src/main/java/project/red/ClientRepository.java
@@ -1,5 +1,10 @@
 package project.red;
 
-public class ClientRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository 
+	extends CrudRepository <Client, Long> {
 
 }
