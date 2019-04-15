4320d4e907998ee6a9654b9af5be39149ffea32b
elmonda <gjekmarkaj.elmonda@gmail.com>
2019-04-15 12:50:40 +0200

    controller e costruttore

--- src/main/java/project/green/GreenHotel.java
+++ src/main/java/project/green/GreenHotel.java
@@ -1,58 +1,72 @@
 package project.green;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "GREEN_HOTELS")
 public class GreenHotel {
-	public GreenHotel() {
-
-	}
-
+	
 	@Id
-	private String hotelId;
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Generate")
+	@SequenceGenerator(sequenceName = "Green_hotel_seq", allocationSize = 1, name = "Generate")
+	private long hotelId;
+	
 	private String hotelName;
 	private String city;
 	private long roomCounter;
+	
+	
+	public GreenHotel(long hotelId, String hotelName, String city, Long roomCounter) {
+		this.hotelId = hotelId;
+		this.hotelName = hotelName;
+		this.city = city;
+		this.roomCounter = roomCounter;
+	}
+	public GreenHotel( String hotelName, String city, Long roomCounter) {
+		this.hotelName = hotelName;
+		this.city = city;
+		this.roomCounter = roomCounter;	
+	}
+	
+	public GreenHotel() {
 
-	public String getHotelId() {
+		
+	}
+	
+	
+	
+	public Long getHotelId() {
 		return hotelId;
 	}
-
-	public void setHotelId(String hotelId) {
+	public void setHotelId(Long hotelId) {
 		this.hotelId = hotelId;
 	}
-
 	public String getHotelName() {
 		return hotelName;
 	}
-
 	public void setHotelName(String hotelName) {
 		this.hotelName = hotelName;
 	}
-
 	public String getCity() {
 		return city;
 	}
-
 	public void setCity(String city) {
 		this.city = city;
 	}
-
 	public long getRoomCounter() {
 		return roomCounter;
 	}
-
 	public void setRoomCounter(long roomCounter) {
 		this.roomCounter = roomCounter;
 	}
-
 	@Override
 	public String toString() {
 		return "GreenHotel [hotelId=" + hotelId + ", hotelName=" + hotelName + ", city=" + city + ", roomCounter="
 				+ roomCounter + "]";
 	}
-
 }
