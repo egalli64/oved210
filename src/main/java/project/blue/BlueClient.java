7b1a8b24d1659dbb3dd20a06a5405bd2c956525e
Monica <monica.conti93@gmail.com>
2019-04-15 10:13:14 +0200

    save changes

--- src/main/java/project/blue/BlueClient.java
+++ src/main/java/project/blue/BlueClient.java
@@ -20,6 +20,13 @@ public class BlueClient {
 	private String email;
 	private Long phone;
 	
+	public BlueClient(long clientId, String clientName, String email, Long phone) {
+		this.clientId = clientId;
+		this.clientName = clientName;
+		this.email = email;
+		this.phone = phone;		
+	}
+	
 	public BlueClient( String clientName, String email, Long phone) {
 		
 		this.clientName = clientName;
