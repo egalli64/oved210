e691417067c0e52ffe39e4396f8fd255a965f65c
elmonda <gjekmarkaj.elmonda@gmail.com>
2019-04-15 16:03:59 +0200

    remove

--- src/main/java/project/green/GreenhotelRepository.java
+++ src/main/java/project/green/GreenhotelRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GreenhotelRepository extends CrudRepository<GreenHotel, String> {
+public interface GreenhotelRepository extends CrudRepository<GreenHotel, Long> {
 }
