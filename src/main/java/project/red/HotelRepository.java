25b0a790fff12244cb5824423175ba182ede5f11
Alessandra <alessandra.cirone@katamail.com>
2019-04-17 11:00:16 +0200

    save 1

--- src/main/java/project/red/HotelRepository.java
+++ src/main/java/project/red/HotelRepository.java
@@ -1,10 +1,12 @@
 package project.red;
 
+
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HotelRepository 
-	extends CrudRepository <Hotel, Long> {
+public interface HotelRepository extends CrudRepository <Hotel, Long> {
+
 
 }
