1776521889fd588818b3d196a5db4599c38be04d
Marina <marinapaolino994@gmail.com>
2019-04-18 14:41:57 +0200

    save repository

--- src/main/java/project/red/ClientRepository.java
+++ src/main/java/project/red/ClientRepository.java
@@ -1,8 +1,13 @@
 package project.red;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends CrudRepository<Client, Long> {
+	
+	@Override
+	List<Client> findAll();
 }
--- src/main/java/project/red/HotelRepository.java
+++ src/main/java/project/red/HotelRepository.java
@@ -2,11 +2,14 @@ package project.red;
 
 
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HotelRepository extends CrudRepository <Hotel, Long> {
-
+	@Override
+	List<Hotel> findAll();
 
 }
