f1bf202115d5302cbb15b07a4999e603c99342a6
Tiziana <tizianaleogrande@gmail.com>
2019-04-18 14:58:05 +0200

    save changes

--- src/main/java/project/red/ClientRepository.java
+++ src/main/java/project/red/ClientRepository.java
@@ -1,13 +1,10 @@
 package project.red;
 
-import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends CrudRepository<Client, Long> {
-	
-	@Override
-	List<Client> findAll();
+
 }
